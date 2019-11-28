de7cf70578aec8409be6491136febda87c6f6be9
Margie Chubin <margie@sproutsocial.com>
2019-11-28 15:23:06 -0600

    bloop

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
             title="Home | Margie"
             keywords={[`accessibility`, `margie`, `developer`, `react`, `a11y`]}
         />
-        <h2>Hi, there, I'm Margie Chubin. I'm a Chicago-based software engineer passionate about web development, accessibility, and good food.</h2>
+        <h2>Hi there, I'm Margie Chubin. I'm a Chicago-based software engineer passionate about web development, accessibility, and good food.</h2>
         I currently work on <a href="https://sproutsocial.com/">Sprout Social</a>'s web team, championing accessibility and creating delightful experiences for our customers.
     </Layout>
 );
