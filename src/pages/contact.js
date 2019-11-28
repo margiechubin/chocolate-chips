7d74e6707503a6ac1f9c5c6948821701dea9b248
Margie Chubin <margie@sproutsocial.com>
2019-11-28 13:45:13 -0600

    add icons to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
+import { IconContext } from "react-icons";
+import { FaTwitter, FaEnvelope, FaLinkedinIn, FaGithub, FaInstagram } from 'react-icons/fa';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
+
+const iconMap = icon => ({
+    Twitter: <FaTwitter />,
+    Email: <FaEnvelope />,
+    LinkedIn: <FaLinkedinIn />,
+    Github: <FaGithub />,
+    Instagram: <FaInstagram />,
+}[icon])
 const ContactPage = () => {
     const contactMethodItem = ({ name, link, msg }) => {
         return (
             <li key={name}>
-                <a href={link}>{name}</a>
+                <a href={link} style={{ textDecoration: 'none', display: 'flex', alignItems: 'center' }}> {iconMap(name)} {name}</a>
             </li>
         );
     };
@@ -32,9 +42,11 @@ const ContactPage = () => {
                     <Layout>
                         <SEO title="Contact" />
                         <h2>Let's connect!</h2>
-                        <ul className="List">
-                            {contact.map(contactMethodItem)}
-                        </ul>
+                        <IconContext.Provider value={{ marginRight: '12px' }}>
+                            <ul className="List">
+                                {contact.map(contactMethodItem)}
+                            </ul>
+                        </IconContext.Provider>
                     </Layout>
                 );
             }}
